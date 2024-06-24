bcef90296901105ced7a287f0ac66137f5ecf6ba
BussyBakks <108082159+BussyBakks@users.noreply.github.com>
2024-06-24 10:25:17 +0700

    main

--- src/main/java/sources/net/thenoppy12/noppyaddon/modules/PanicModule.java
+++ src/main/java/sources/net/thenoppy12/noppyaddon/modules/PanicModule.java
@@ -1,19 +1,23 @@
 package sources.net.thenoppy12.noppyaddon.modules;
 
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import sources.net.thenoppy12.noppyaddon.NoppyInit;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.orbit.EventHandler;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
+import sources.net.thenoppy12.noppyaddon.NoppyInit;
+
+// works
 public class PanicModule extends Module {
     public PanicModule() {
         super(NoppyInit.CATEGORY, "Panic", "Instantly turn all active modules off, caution with this one, use to proof that u dont hack =] .");
     }
-    @EventHandler
-    private void Panic() {
-        for (Module module : Modules.get().getAll())
+    @Override
+    public void onActivate() {
+        for (Module module : Modules.get().getAll()) {
             if (module.isActive() && module != this) {
                 module.toggle();
             }
+        }
+        ChatUtils.info("All modules toggled");
         this.toggle();
     }
 }
